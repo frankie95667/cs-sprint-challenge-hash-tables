464198e936db496aba8185629561d4d5809bed2b
Anthony Kawa <anthonyk2020@gmail.com>
2020-08-07 10:28:50 -0700

    completed ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,10 +3,20 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     result = []
+    hashtable = {}
     # loop over array of arrays
     for i in range(len(arrays)):
         for j in range(len(arrays[i])):
-            
+            # add all elements in the first array of arrays as keys in the hashtable and set value to 1
+            if i == 0:
+                hashtable[arrays[i][j]] = 1
+            # check if key is in hashtable at the current elements in the subarray
+            elif arrays[i][j] in hashtable:
+                # increment value by 1
+                hashtable[arrays[i][j]] += 1
+
+    # add keys to array that have values that equal the arrays length
+    result = [key for key, value in hashtable.items() if value == len(arrays)]
 
     return result
 
